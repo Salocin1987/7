d600589d395b3c4990df0c4f314600461fd04ffb
Salocin1987 <72270336+Salocin1987@users.noreply.github.com>
2021-01-14 02:30:22 +0100

    Add files via upload

---
+++ qgis2web_2021_01_13-19_16_13_417215/data/Vidos_1.js
@@ -0,0 +1,10 @@
+var json_Vidos_1 = {
+"type": "FeatureCollection",
+"name": "Vidos_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "nom": "Blacwater river" }, "geometry": { "type": "Point", "coordinates": [ -86.880497730358016, 30.705982572189725 ] } },
+{ "type": "Feature", "properties": { "id": "3", "nom": "En ville (NOLA)" }, "geometry": { "type": "Point", "coordinates": [ -90.06729457812439, 29.951924506865446 ] } },
+{ "type": "Feature", "properties": { "id": "2", "nom": "Morrison springs" }, "geometry": { "type": "Point", "coordinates": [ -85.903945383978112, 30.657872191008423 ] } }
+]
+}
